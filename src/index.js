be02b0cdb9db2b9e7442a7b2ff044c365998af5a
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-05-18 21:17:21 +0530

    Initial project setup

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { recoverTodosFromLocalStorage } from "./todos";
-import { recoverProjectsFromLocalStorage } from "./projects";
-import { showTodosList, showProjectList } from "./gui";
-import "./style.css";
-
-recoverTodosFromLocalStorage();
-recoverProjectsFromLocalStorage();
-showTodosList("all");
-showProjectList();
