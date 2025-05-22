e838a1de22bff39c519552988dae94e961db653b
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-05-22 15:54:17 +0000

    Add toggle buttons for unit system

--- src/gui.js
+++ src/gui.js
@@ -1,9 +1,13 @@
 export const searchInput = document.querySelector('input#search-input');
 export const searchButton = document.querySelector('button#search-button');
-export const locationDisplay = document.querySelector('div#location-display');
-const dailyWeatherTableArray = [[], [], []];
+export const locationDisplay = document.querySelector('div#location-display span');
+export const weatherIcon = document.querySelector('#weather-icon');
+export const metricToggle = document.querySelector('#metric');
+export const usToggle = document.querySelector('#us');
 
-for(let i = 0; i < 3; i++){
+const dailyWeatherTableArray = [[], [], [], []];
+
+for(let i = 0; i < 4; i++){
     for(let j = 0; j < 4; j++){
         dailyWeatherTableArray[i][j] = document.querySelector(`table#daily-weather-table > tbody > tr:nth-child(${i + 1}) > :nth-child(${j + 1})`);
     }
@@ -12,5 +16,3 @@ for(let i = 0; i < 3; i++){
 export function getCellFromDailyWeatherTable(row, column){
     return dailyWeatherTableArray[row][column];
 }
-
-console.log(dailyWeatherTableArray);
--- src/weatherdata.js
+++ src/weatherdata.js
@@ -1,9 +1,5 @@
-//Following import is for test purpose only
-// import sampleResult from './sampleresult.json';
-//Above import is for test purpose only
-
 import { getCellFromDailyWeatherTable, searchButton, searchInput } from './gui';
-import { locationDisplay } from './gui';
+import { locationDisplay, weatherIcon, metricToggle, usToggle } from './gui';
 
 async function getWeather(location, unitSystem) {
   try {
@@ -17,23 +13,34 @@ async function getWeather(location, unitSystem) {
 }
 
 export async function showWeather(locationInput, unitSystemInput) {
-  try{
-  const data = await getWeather(locationInput, unitSystemInput);
+  try {
+    const data = await getWeather(locationInput, unitSystemInput);
 
-  // Following declaration is for test purpose only
-  // const data = sampleResult;
-  // Above declaration is for test purpose only
+    locationDisplay.textContent = data.resolvedAddress;
+    const weatherIconImage = await import(`./weather-icons/${data.days[0].icon}.png`);
+    weatherIcon.src = weatherIconImage.default;
 
-  locationDisplay.textContent = data.resolvedAddress;
-  
-  getCellFromDailyWeatherTable(0, 0).textContent = data.days[0].datetime;
-  getCellFromDailyWeatherTable(0, 1).textContent = data.days[0].tempmin;
-  getCellFromDailyWeatherTable(0, 2).textContent = data.days[0].tempmax;
-  getCellFromDailyWeatherTable(0, 3).textContent = data.days[0].precip;
+    getCellFromDailyWeatherTable(0, 0).textContent = 'Today';
+    getCellFromDailyWeatherTable(0, 1).textContent = data.days[0].tempmin;
+    getCellFromDailyWeatherTable(0, 2).textContent = data.days[0].tempmax;
+    getCellFromDailyWeatherTable(0, 3).textContent = data.days[0].precip;
 
+    getCellFromDailyWeatherTable(1, 0).textContent = data.days[1].datetime;
+    getCellFromDailyWeatherTable(1, 1).textContent = data.days[1].tempmin;
+    getCellFromDailyWeatherTable(1, 2).textContent = data.days[1].tempmax;
+    getCellFromDailyWeatherTable(1, 3).textContent = data.days[1].precip;
 
+    getCellFromDailyWeatherTable(2, 0).textContent = data.days[2].datetime;
+    getCellFromDailyWeatherTable(2, 1).textContent = data.days[2].tempmin;
+    getCellFromDailyWeatherTable(2, 2).textContent = data.days[2].tempmax;
+    getCellFromDailyWeatherTable(2, 3).textContent = data.days[2].precip;
+
+    getCellFromDailyWeatherTable(3, 0).textContent = data.days[3].datetime;
+    getCellFromDailyWeatherTable(3, 1).textContent = data.days[3].tempmin;
+    getCellFromDailyWeatherTable(3, 2).textContent = data.days[3].tempmax;
+    getCellFromDailyWeatherTable(3, 3).textContent = data.days[3].precip;
   }
-  catch(error){
+  catch (error) {
     console.log('Error in showWeather', error);
   }
 }
@@ -47,6 +54,16 @@ searchButton.addEventListener('click', (event) => {
   showWeather(currentLocation, currentUnitSystem);
 });
 
-//Add one switch for changing unit system which will:
-// 1. Check if currentLocation is true
-// 2. If currentLocation is true, will call showWeather with current location and seleted unit system ('metric'/ 'us')
+metricToggle.addEventListener("click", () => {
+  if(currentUnitSystem === 'us'){
+    currentUnitSystem = 'metric';
+  showWeather(currentLocation, currentUnitSystem);
+  }
+})
+
+usToggle.addEventListener("click", () => {
+  if(currentUnitSystem === 'metric'){
+    currentUnitSystem = 'us';
+  showWeather(currentLocation, currentUnitSystem);
+  }
+})
